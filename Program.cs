2ef5be2c9ccf3f2e5986cd85f9abbc73d9aa9c4c
dasha <dasha15042009@yandex.ru>
2022-08-21 19:30:33 +0300

    3ex

--- Program.cs
+++ Program.cs
@@ -16,16 +16,29 @@
 //
 //
 //Second ex
-Console.WriteLine("Введите первое число: ");
+//Console.WriteLine("Введите первое число: ");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите второе число: ");
+//int b = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите третье число: ");
+//int c = int.Parse(Console.ReadLine());
+//int max = a;
+//if(b > max)
+//{max =b;}
+//if(c > max)
+//{max = c;}
+//Console.Write("Максимальное число: ");
+//Console.Write(max);
+//
+//
+//Third ex
+Console.WriteLine("Введите число: ");
 int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int b = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите третье число: ");
-int c = int.Parse(Console.ReadLine());
-int max = a;
-if(b > max)
-{max =b;}
-if(c > max)
-{max = c;}
-Console.Write("Максимальное число: ");
-Console.Write(max);
+if(a%2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.Write("No");
+}
